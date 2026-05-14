b4fa6af5aab05c3bec4851ce543f1a25185458c5
Paweł Lysko <lyskopawel03@gmail.com>
2026-05-14 02:17:29 +0200

    fix typo in url shortener query handler

--- src/UrlShortener/Features/GetLinkByCode/GetLinkByCodeQueryHandler.cs
+++ src/UrlShortener/Features/GetLinkByCode/GetLinkByCodeQueryHandler.cs
@@ -8,7 +8,7 @@ namespace UrlShortener.Features.GetLinkByCode;
 public sealed class GetLinkByCodeQueryHandler(AppDbContext context) : IQueryHandler<GetLinkByCodeQuery, Result<GetLinkByCodeQueryResult>>
 {
     private readonly AppDbContext _context = context;
-    2 
+
     public async Task<Result<GetLinkByCodeQueryResult>> Handle(GetLinkByCodeQuery query, CancellationToken cancellationToken)
     {
         var result = await _context.Urls.FirstOrDefaultAsync(x => x.ShortUrl == query.Code, cancellationToken);
