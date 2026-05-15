1524eba73a076774348b0b642a005b20be0fd3a9
Paweł Lysko <lyskopawel03@gmail.com>
2026-05-15 02:02:08 +0200

    Add Hybrid cache witch l2 redis Implement on GetLinkByCode feature

--- src/UrlShortener/Features/GetLinkByCode/GetLinkByCodeQueryHandler.cs
+++ src/UrlShortener/Features/GetLinkByCode/GetLinkByCodeQueryHandler.cs
@@ -1,23 +1,32 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Hybrid;
 using UrlShortener.Abstractions.Messaging;
 using UrlShortener.Infrastructure;
 using UrlShortener.Results;
 
 namespace UrlShortener.Features.GetLinkByCode;
 
-public sealed class GetLinkByCodeQueryHandler(AppDbContext context) : IQueryHandler<GetLinkByCodeQuery, Result<GetLinkByCodeQueryResult>>
+public sealed class GetLinkByCodeQueryHandler(AppDbContext context, HybridCache cache) : IQueryHandler<GetLinkByCodeQuery, Result<GetLinkByCodeQueryResult>>
 {
     private readonly AppDbContext _context = context;
 
-    public async Task<Result<GetLinkByCodeQueryResult>> Handle(GetLinkByCodeQuery query, CancellationToken cancellationToken)
+    public async Task<Result<GetLinkByCodeQueryResult>> Handle(GetLinkByCodeQuery query, CancellationToken ct)
     {
-        var result = await _context.Urls.AsNoTracking().FirstOrDefaultAsync(x => x.ShortUrl == query.Code, cancellationToken);
+        string? longUrl = await cache.GetOrCreateAsync(
+            $"longurl:{query.Code}",
+            async token =>
+            {
+                var result = await _context.Urls.AsNoTracking().FirstOrDefaultAsync(x => x.ShortUrl == query.Code, token);
+                return result?.LongUrl;
+            },
+            cancellationToken: ct
+            );
         
-        if (result is null)
+        if (longUrl is null)
         {
             return Result<GetLinkByCodeQueryResult>.Failure(GetLinkByCodeErrors.NotFound);
         }
 
-        return Result<GetLinkByCodeQueryResult>.Success(new GetLinkByCodeQueryResult(LongUrl: result.LongUrl));
+        return Result<GetLinkByCodeQueryResult>.Success(new GetLinkByCodeQueryResult(longUrl));
     }
 }
